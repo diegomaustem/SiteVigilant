5e461ff10bb2e5f24084d200a015a39313b06158
diegomaustem <diego.maustem@gmail.com>
2026-06-04 09:39:47 -0300

    :sparkles: feat: Implementing methods of getAll and create monitor in controller

---
+++ src/modules/monitor/monitor.controller.ts
@@ -0,0 +1,28 @@
+import type { Request, Response } from 'express';
+import { MonitorService } from './monitor.service.js';
+import type { InputMonitor } from './monitor.types.js'; 
+
+export class MonitorController {
+    private readonly monitorService: MonitorService;
+
+    constructor(monitorService: MonitorService) {
+        this.monitorService = monitorService;
+    }
+
+    getAll = async (req: Request, res: Response): Promise<void> => {
+        const monitors = await this.monitorService.getAll();
+        res.status(200).json({ success: true, data: monitors });
+    }
+
+    create = async (req: Request, res: Response): Promise<void> => {
+        const monitorData : InputMonitor = {
+            periodicityId: req.body.periodicityId,
+            name: req.body.name,
+            description: req.body.description,
+            url: req.body.url
+        };
+
+        const createdMonitor = await this.monitorService.create(monitorData); 
+        res.status(201).json({ success: true, data: createdMonitor });
+    }   
+}

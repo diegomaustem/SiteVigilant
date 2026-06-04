a81f608c20434d774324a2f88ded9436a712f187
diegomaustem <diego.maustem@gmail.com>
2026-06-04 09:38:15 -0300

    :sparkles: feat: Create types of monitor

---
+++ src/modules/monitor/monitor.types.ts
@@ -0,0 +1,26 @@
+export interface InputMonitor {
+    periodicityId: number;
+    name: string;
+    description: string;
+    url: string;    
+}
+
+export interface CreatedMonitor {
+    id: number;
+    periodicityId: number;
+    name: string;
+    description: string;
+    url: string;
+    createdAt : Date;
+    updatedAt: Date;
+}
+
+export interface ListCreatedMonitor {
+    id: number;
+    periodicity_id: number;
+    name: string;
+    description: string;
+    url: string;
+    created_at : Date;
+    updated_at: Date;
+}

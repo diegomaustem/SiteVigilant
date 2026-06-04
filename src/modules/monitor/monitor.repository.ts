a77807dea3d1ae3cb4f45d4e8a7cdb73eb8e0c50
diegomaustem <diego.maustem@gmail.com>
2026-06-04 09:36:08 -0300

    :sparkles: feat: Implementing methods of getAll and create monitor in repository

---
+++ src/modules/monitor/monitor.repository.ts
@@ -0,0 +1,37 @@
+import type { Knex } from 'knex';
+import type { CreatedMonitor, InputMonitor, ListCreatedMonitor } from './monitor.types.js';
+
+export class MonitorRepository {
+  private db: Knex;
+  private readonly monitorTable = 'monitor';
+
+  constructor(db: Knex) {
+    this.db = db;
+  }
+
+  async getAll(): Promise<ListCreatedMonitor[]>{
+    try {
+      return await this.db<ListCreatedMonitor>(this.monitorTable).select('*');
+    } catch (error: any) {
+      console.error(`[MonitorRepository.getActiveMonitors] Erro ao buscar monitores: ${error.message}`);
+      throw error;
+    }
+  }
+
+  async create(inputMonitor: InputMonitor): Promise<CreatedMonitor> {
+    try {
+      const [newMonitor] = await this.db(this.monitorTable)
+        .insert({
+          periodicity_id: inputMonitor.periodicityId,
+          name: inputMonitor.name,
+          description: inputMonitor.description,
+          url: inputMonitor.url
+        })
+        .returning('*'); 
+      return newMonitor;
+    } catch (error: any) {
+      console.error(`[MonitorRepository.create] Erro ao inserir monitor: ${error.message}`);
+      throw error;
+    }
+  }
+}

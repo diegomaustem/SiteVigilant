ba8228d422c89f66eea2cd023ca509ec440aac6b
diegomaustem <diego.maustem@gmail.com>
2026-06-09 12:12:51 -0300

    :sparkles: feat: Create new verify if already exist monitor

--- src/modules/monitor/monitor.service.ts
+++ src/modules/monitor/monitor.service.ts
@@ -1,6 +1,6 @@
+import { ConflictError } from '../../utils/errors.js';
 import { MonitorRepository } from './monitor.repository.js';
 import type { InputMonitor, CreatedMonitor, ListMonitors } from './monitor.types.js';
-
 export class MonitorService {
 
   private readonly monitorRepository: MonitorRepository;
@@ -10,11 +10,21 @@ export class MonitorService {
   }
 
   async getAll(): Promise<ListMonitors[]> { 
-    return await this.monitorRepository.getAll();
+    try {
+      return await this.monitorRepository.getAll();
+    } catch(error: any) {
+      console.error(`[MonitorService.getAll] Erro ao lista monitores: ${error.message}` );
+      throw error;
+    }
   }
 
   async create(inputMonitor: InputMonitor): Promise<CreatedMonitor> {
     try {
+      const existingMonitor = await this.monitorRepository.getByName(inputMonitor.name); 
+      if (existingMonitor) {
+        throw new ConflictError('Já existe um monitor cadastrado com este nome. Escolha outro, por favor.');
+      }
+
       return await this.monitorRepository.create(inputMonitor);
     }catch(error: any) {
       console.error(`[MonitorService.create] Erro ao criar monitor: ${error.message}`);

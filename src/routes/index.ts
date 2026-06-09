6d88a29c568806d99027c2f319e3718e690a6526
diegomaustem <diego.maustem@gmail.com>
2026-06-09 12:19:07 -0300

    :sparkles: feat: Adding async handler for routes archive

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import type { Request, Response } from 'express';
+import { asyncHandler } from '../middlewares/async-handler.js';
 
 import { monitorController } from '../config/container.js';
 import { MonitorValidator } from '../middlewares/monitor-validator.middleware.js';
@@ -12,8 +13,8 @@ router.get('/health', (req: Request, res: Response) => {
   res.status(200).json({ status: 'API running', timestamp: new Date() });
 });
 
-router.get('/list-monitors', monitorController.getAll);
-router.post('/create-monitor', MonitorValidator.validateCheckInput, monitorController.create);
+router.get('/list-monitors', asyncHandler(monitorController.getAll));
+router.post('/create-monitor', MonitorValidator.validateCheckInput, asyncHandler(monitorController.create));
 
 router.get('/list-periodicities', periodicityController.getAll); 
 router.post('/create-periodicity', periodicityController.create);

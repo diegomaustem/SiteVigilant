f4239c498f76e98230911d182025df43d5d02256
diegomaustem <diego.maustem@gmail.com>
2026-06-17 22:48:31 -0300

    :recycle: refactor: Adjustment type of monitor

--- src/modules/monitor/monitor.types.ts
+++ src/modules/monitor/monitor.types.ts
@@ -5,26 +5,6 @@ export interface InputMonitor {
     url: string;    
 }
 
-export interface CreatedMonitor {
-    id: number;
-    periodicityId: number;
-    name: string;
-    description: string;
-    url: string;
-    createdAt : Date;
-    updatedAt: Date;
-}
-
-export interface ListMonitors {
-    id: number;
-    periodicity_id: number;
-    name: string;
-    description: string;
-    url: string;
-    created_at : Date;
-    updated_at: Date;
-}
-
 export interface Monitor {
     id: number;
     periodicityId: number;

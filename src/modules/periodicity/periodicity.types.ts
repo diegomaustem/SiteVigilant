8f2d8eded23cebddab55cc79575bf37588ed4ad9
diegomaustem <diego.maustem@gmail.com>
2026-06-18 09:03:05 -0300

    :sparkles: feat: Adjustment of types periodicity

---
+++ src/modules/periodicity/periodicity.types.ts
@@ -0,0 +1,12 @@
+export interface InputPeriodicity {
+    time: string
+    status: boolean
+}
+
+export interface Periodicity {
+    id: number;
+    time: string
+    status: boolean
+    createdAt : Date;
+    updatedAt: Date;
+}

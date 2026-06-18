7a8ea2efcfbf5dfd8ae5adb6d27de64f664499f0
diegomaustem <diego.maustem@gmail.com>
2026-06-18 09:05:28 -0300

    :sparkles: feat: Adjustment method of get by id for periodicity

---
+++ src/modules/periodicity/periodicity.controller.ts
@@ -0,0 +1,36 @@
+import type { Request, Response } from 'express';
+import { PeriodicityService } from './periodicity.service.js';
+import type { InputPeriodicity } from './periodicity.types.js';
+import { BadRequestError } from '../../utils/errors.js';
+
+export class PeriodicityController {
+    private readonly periodicityService: PeriodicityService;
+
+    constructor(periodicityService: PeriodicityService) {
+        this.periodicityService = periodicityService;
+    }
+
+    getAll = async (req: Request, res: Response): Promise<void> => {
+        const periodicities = await this.periodicityService.getAll();
+        res.json({ success: true, data: periodicities });
+    } 
+
+    getById = async (req: Request, res: Response): Promise<void> => {
+        const id = Number(req.params.id);
+        if (isNaN(id) || id <= 0) {
+            throw new BadRequestError('ID inválido. Deve ser um número inteiro.');
+        }
+        const periodicity = await this.periodicityService.getById(id);
+        res.status(200).json({ success: true, data: periodicity });
+    }
+
+    create = async (req: Request, res: Response): Promise<void> => {
+        const periodicityData : InputPeriodicity = {
+            time: req.body.time,
+            status: req.body.status
+        };
+
+        const createdPeriodicity = await this.periodicityService.create(periodicityData);
+        res.status(201).json({ success: true, data: createdPeriodicity });
+    }
+}

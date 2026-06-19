82aa3b56bd4a974963de0ec8754b540a8e990e39
diegomaustem <diego.maustem@gmail.com>
2026-06-19 19:38:24 -0300

    :sparkles: feat: Creating monitor jobs

---
+++ src/jobs/monitor.job.ts
@@ -0,0 +1,22 @@
+import cron from 'node-cron';
+import chalk from 'chalk';
+import { monitorService } from '../config/container.js';
+import { urlConsultantService } from '../config/container.js';
+
+export function startMonitorJobs() {
+  cron.schedule('*/10 * * * * *', async () => {
+    console.log(chalk.green(`[${new Date().toISOString()}] Iniciando verificação de monitores. 🟢`));
+    try {
+        const monitors = await monitorService.getAll();
+
+        for (const monitor of monitors) {
+            const checkMonitor = await urlConsultantService.checkAddress(monitor);
+            await urlConsultantService.saveCheckAddress(checkMonitor);
+        }
+
+      console.log(chalk.green('Verificação concluída com sucesso. ✅'));
+    } catch (error) {
+      console.error(chalk.red.bold('Erro ao verificar monitores:', error));
+    }
+  });
+}

8a51eb3342562d7a1a33479b6a08d4bf63880044
diegomaustem <diego.maustem@gmail.com>
2026-06-19 20:34:46 -0300

    :sparkles: feat: Adding init start monitor jobs

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import router from './routes/index.js';
 import { errorHandler } from './middlewares/error-handler.js';
+import { startMonitorJobs } from './jobs/monitor.job.js';
 
 const app: express.Application = express();
 
@@ -9,6 +10,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', router);
+
+startMonitorJobs();
 
 app.use(errorHandler);
 

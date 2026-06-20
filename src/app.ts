85d0c2fbd26689192b3b6d35c60d1a4d199a7af7
diegomaustem <diego.maustem@gmail.com>
2026-06-20 12:36:20 -0300

    :sparkles: feat: Adding use general limiter of index

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,15 @@ import express from 'express';
 import router from './routes/index.js';
 import { errorHandler } from './middlewares/error-handler.js';
 import { startMonitorJobs } from './jobs/monitor.job.js';
+import { generalLimiter } from './middlewares/rate-limit.middleware.js';
 
 const app: express.Application = express();
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
+
+app.use(generalLimiter);
 
 app.use('/api', router);
 

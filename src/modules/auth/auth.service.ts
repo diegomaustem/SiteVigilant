7c9a3425f506da5a50735b4906b007533e094b46
diegomaustem <diego.maustem@gmail.com>
2026-06-27 19:54:16 -0300

    :sparkles: feat: Adjustment of error handler in register

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -2,9 +2,8 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import { UserRepository } from '../user/user.repository.js';
 import type { UserResponse } from '../user/user.types.js';
-import { UnauthorizedError, ConflictError } from '../../utils/errors.js';
+import { UnauthorizedError, ConflictError, NotFoundError } from '../../utils/errors.js';
 import { ENV } from '../../config/env.js';
-
 export class AuthService {
     private readonly userRepository: UserRepository;
 
@@ -15,27 +14,20 @@ export class AuthService {
     async register(email: string, password: string, name: string): Promise<UserResponse> {
         const existingUser = await this.userRepository.getByEmail(email);
         if (existingUser) {
-        throw new ConflictError('E-mail já cadastrado. Por favor, tente outro.');
+            throw new ConflictError('E-mail já cadastrado. Por favor, tente outro.');
         }
-
-        const user = await this.userRepository.create({ email, password, name });
-        return {
-            id: user.id,
-            email: user.email,
-            name: user.name,
-            createdAt: user.createdAt,
-        };
+        return await this.userRepository.create({ email, password, name });
     }
 
     async login(email: string, password: string): Promise<{ user: UserResponse; token: string }> {
         const user = await this.userRepository.getByEmail(email);
         if (!user) {
-            throw new UnauthorizedError('Usuário não encontrado');
+            throw new NotFoundError('Usuário não encontrado.');
         }
 
         const isPasswordValid = await bcrypt.compare(password, user.passwordHash);
         if (!isPasswordValid) {
-            throw new UnauthorizedError('Credenciais inválidas.Veifique seu usuário e senha');
+            throw new UnauthorizedError('Credenciais inválidas. Veifique seu usuário e senha.');
         }
 
         const token = jwt.sign(
@@ -48,6 +40,7 @@ export class AuthService {
         user: {
             id: user.id,
             email: user.email,
+            role: user.role,
             name: user.name,
             createdAt: user.createdAt,
         },
@@ -55,7 +48,7 @@ export class AuthService {
         };
     }
 
-    verifyToken(token: string): { userId: number; email: string; name: string } {
+    verifyToken(token: string): { userId: number; email: string; name: string, role: string } {
         try {
             return jwt.verify(token, ENV.JWT_SECRET) as any;
         } catch {

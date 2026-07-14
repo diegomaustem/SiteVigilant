6f2c5c5f3dd4077ebd9d969b17e8fc7b8530d5ae
diegomaustem <diego.maustem@gmail.com>
2026-07-14 20:23:03 -0300

    :recycle: refactor: Adjustment of methods service

--- src/modules/periodicity/periodicity.service.ts
+++ src/modules/periodicity/periodicity.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestError, ConflictError, NotFoundError } from '../../utils/errors.js';
+import { BadRequestError, ConflictError } from '../../utils/errors.js';
 import { PeriodicityRepository } from './periodicity.repository.js';
 import type { InputPeriodicity, Periodicity } from './periodicity.types.js';
 
@@ -15,50 +15,33 @@ export class PeriodicityService {
     return await this.periodicityRepository.getAll();
   }
 
-  async getById(periodicityId: number): Promise<Periodicity | undefined> {
-      const periodicity = await this.periodicityRepository.getById(periodicityId);
-      if(!periodicity) {
-        throw new NotFoundError(`Periodo com ID ${periodicityId} não encontrado.`);
-      }
-      return periodicity;
+  async getById(periodicityId: number): Promise<Periodicity> {
+    return this.periodicityRepository.getById(periodicityId);
   }
 
   async create(inputPeriodicity: InputPeriodicity): Promise<Periodicity> {
-    try {
-      const existingPeriodicity = await this.periodicityRepository.getByTime(inputPeriodicity.time); 
-      if (existingPeriodicity) {
-        throw new ConflictError('Já existe um periodo cadastrado com este valor. Escolha outro, por favor.');
-      }
-      
-      return await this.periodicityRepository.create(inputPeriodicity);
-    }catch(error: any) {
-      if (!(error instanceof ConflictError)) {
-        console.error(`[RepositoryService.create] Erro inesperado: ${error.message}`);
-      }
-      throw error;
+    const existingPeriodicity = await this.periodicityRepository.getByTime(inputPeriodicity.time); 
+    if (existingPeriodicity) {
+      throw new ConflictError('Já existe um periodo cadastrado com este valor. Escolha outro, por favor.');
     }
+      
+    return await this.periodicityRepository.create(inputPeriodicity);
   }
 
   async update(id: number, data: InputPeriodicity): Promise<Periodicity> {
     if (isNaN(id) || id <= 0) {
       throw new BadRequestError('ID inválido. Deve ser um número inteiro.');
     }
-  
-    const existingPeriodicity = await this.periodicityRepository.getById(id);
-    if (!existingPeriodicity) throw new NotFoundError('Periodicidade não encontrada para atualização.');
       
     const periodicityWithSameTime = await this.periodicityRepository.getByTime(data.time);
     if (periodicityWithSameTime && periodicityWithSameTime.id !== id) {
       throw new ConflictError('Já existe uma periodicidade cadastrada com este valor. Escolha outro, por favor.');
     }
   
-    return await this.periodicityRepository.update(id, data);
+    return this.periodicityRepository.update(id, data);
   }
   
-  async delete(id: number): Promise<void> {
-    const existingPeriodicity = await this.periodicityRepository.getById(id); 
-    if(!existingPeriodicity) throw new NotFoundError('Periodicidade não encontrada para deleção.');
-              
-    await this.periodicityRepository.delete(id);
+  async delete(id: number): Promise<void> {       
+    return this.periodicityRepository.delete(id);
   }
 }
